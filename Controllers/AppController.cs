087733bf09d9d7dde2ee540e698ac1b163c2d89f
danapt <apetridaniel3@gmail.com>
2022-06-16 23:10:17 +0200

    ApplicationAsp.NetCore+javaScript+MVC+Html+CSS

---
+++ Controllers/AppController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineMotorWorld.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            // throw new InvalidProgramException("Bad things happen to goof people!");
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        { 
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(object model)
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,38 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+$(document).ready(function () {
+
+    var x = 0;
+    var s = "";
+    //alert("hello God! ");
+    console.log("Whats up lad? ");
+
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+
+    var button =$("#buyButton");
+    button.on("click", function () {
+        console.log("Buying Motorbikes");
+    })
+
+    var productInfo = $(".product-p li");
+    productInfo.on("click", function () {
+        console.log("You clicked on" + $
+            (this).text());
+
+    });
+
+
+
+    var $loginToggle = $("#logginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.slideToggle(1000);
+    });
+
+});
